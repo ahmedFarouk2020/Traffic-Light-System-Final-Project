ba2b4c8019e09e8d3f2bdc2f6c265e296b505726
Ahmed Farouk <ahmedfarook0111@gmail.com>
2022-05-23 19:25:10 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -5,15 +5,11 @@
 int main()
 {
     DIO_Init();
-//    Led_Init();
     SSD_Init();
-        SSD_MainFunction();
 
     while(1)
     {
-//        Led_Color = 0;
-//        Led_MainFunction();
-//        DIO_ChannelWrite(5, 1);
+        SSD_MainFunction();
     }
     return 0;
 }
